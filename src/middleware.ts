ccaa856e33e4a398c2717ea621140fc7ea3f9d4a
srodi <sacha.rodier@aucotec.com>
2025-01-13 22:16:17 +0100

    ghetto link previews?

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,10 +19,33 @@ export const onRequest = defineMiddleware((context, next) => {
     }
   }
 
-  return new Response("Auth required", {
-    status: 401,
-    headers: {
-      "WWW-authenticate": 'Basic realm="Secure Area"',
-    },
-  });
+  return new Response(
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="generator" content="Astro v5.1.6">
+        <title>Chez Superdad</title>
+      </head>
+      <body>
+        <p>Auth required</p>
+      </body>
+    </html>
+    `,
+    {
+      status: 401,
+      headers: {
+        "WWW-authenticate": 'Basic realm="Secure Area"',
+        "Content-Type": "text/html",
+      },
+    }
+  );
 });
