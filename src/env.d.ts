bdff9f85d4308bb9a1ee6088e725dcb7b4dd6461
srodi <sacha.rodier@aucotec.com>
2025-01-13 21:39:47 +0100

    add auth

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="astro/client-image" />
 
 interface ImportMetaEnv {
-	readonly PUBLIC_VERCEL_ANALYTICS_ID: string;
+  readonly PUBLIC_VERCEL_ANALYTICS_ID: string;
+  readonly USERNAME: string;
+  readonly PW: string;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
