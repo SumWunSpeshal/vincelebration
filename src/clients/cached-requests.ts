e062e8b5998a26d97e25c541d256d8d0cf4df8d9
srodi <sacha.rodier@aucotec.com>
2025-01-17 23:08:26 +0100

    Finally figured out caching

---
+++ src/clients/cached-requests.ts
@@ -0,0 +1,24 @@
+import { cloudinary, type CloudinaryResponse } from "./cloudinary";
+
+const cache: {
+  imgs: CloudinaryResponse | null;
+} = {
+  imgs: null,
+};
+
+export async function getFromCache<
+  T extends (typeof cache)[keyof typeof cache],
+>(key: keyof typeof cache, cb: () => Promise<NonNullable<T>>) {
+  cache[key] ??= await cb();
+  return cache[key];
+}
+
+export async function getImages() {
+  return await getFromCache("imgs", async () => {
+    return await cloudinary.search
+      .expression("resource_type:image")
+      .with_field("context")
+      .max_results(100)
+      .execute();
+  });
+}
