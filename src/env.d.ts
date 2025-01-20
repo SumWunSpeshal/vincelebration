f5de853f95b56236f2792b29b6f43fdde57cb558
srodi <sacha.rodier@aucotec.com>
2025-01-20 16:49:38 +0100

    connect turso db

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,6 +8,8 @@ interface ImportMetaEnv {
   readonly PUBLIC_CLOUDINARY_API_KEY: string;
   readonly CLOUDINARY_API_SECRET: string;
   readonly CLOUDINARY_URL: string;
+  readonly ASTRO_DB_REMOTE_URL: string;
+  readonly ASTRO_DB_APP_TOKEN: string;
 }
 
 interface ImportMeta {
