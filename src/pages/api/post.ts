4512dd4894c80ac5144af134fa2b854b16c667fd
srodi <sacha.rodier@aucotec.com>
2025-02-01 17:01:12 +0100

    fix typo

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -1,12 +1,12 @@
-import type { APIRoute } from "astro";
-import { Post, db } from "astro:db";
+import type { APIRoute } from "astro"
+import { Post, db } from "astro:db"
 
 export const POST: APIRoute = async ({ request }) => {
-  const body: { username: string; comment: string } = await request.json();
+  const body: { username: string; content: string } = await request.json();
 
   await db.insert(Post).values({
     author: body.username,
-    content: body.comment,
+    content: body.content,
   });
 
   return new Response(JSON.stringify({ success: true }), { status: 200 });
