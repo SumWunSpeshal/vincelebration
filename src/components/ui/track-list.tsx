20f305ee3724540dbe7b8ef268e6d84f4f3fd940
srodi <sacha.rodier@aucotec.com>
2025-02-02 22:45:39 +0100

    change track-list styling

--- src/components/ui/track-list.tsx
+++ src/components/ui/track-list.tsx
@@ -16,7 +16,7 @@ export function TrackList({
       {grouped.map(([album, tracks]) => {
         return (
           <li className="space-y-6">
-            <h2 className="text-3xl uppercase">{album}</h2>
+            <h2 className="text-3xl uppercase text-gray-700">{album}</h2>
             {tracks?.[0]?.cover && (
               <div className="max-w-80 max-h-80">
                 <img
@@ -32,12 +32,12 @@ export function TrackList({
                   <button
                     type="button"
                     onClick={() => onSelect(id)}
-                    className={`text-blue-900 text-lg font-medium flex hover:bg-green-50 rounded-sm px-2 py-2 w-full transition-colors ${current?.id === id && "!bg-green-200"}`}
+                    className={`text-lg flex hover:bg-green-50 rounded-sm px-2 py-2 w-full transition-colors ${current?.id === id && "!bg-green-200"}`}
                   >
-                    <div className="w-12 font-mono">
+                    <div className="w-12 font-mono text-green-800 font-medium">
                       {current?.id === id ? "▶" : `${i + 1}`.padStart(2, "0")}
                     </div>
-                    <span>{title}</span>
+                    <span className="text-gray-700">{title}</span>
                   </button>
                 </li>
               ))}
